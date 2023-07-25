0da41ab21f51e9085c082364dbe2a01e35447cd1
Green0-0 <138409197+Green0-0@users.noreply.github.com>
2023-07-25 09:57:56 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -29,6 +29,7 @@ async def on_ready():
     await bot.tree.sync()
     for w in await guild.webhooks():
         await w.delete()
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="/help for my commands, and ping me to talk!"))
     print(f'Logged in as {bot.user}, begin using the bot!')
     
 bot.run(data.TOKEN)
